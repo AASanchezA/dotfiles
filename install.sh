a13c7f7838c2a06a41f41595105ab740b809324d
AASanchezA <andres-sanchez@gmx.net>
2014-05-29 14:48:33 +0200

    Added git-completion.sh and git-prompt.sh to install.sh

--- install.sh
+++ install.sh
@@ -33,3 +33,5 @@ done
 # linking also bash alias file
 echo "Creating symlink to bash_aliases in home directory."
 ln -s $dir/bash_aliases ~/.bash_aliases
+ln -s $dir/git-completion.sh ~/.git-completion.sh
+ln -s $dir/git-prompt.sh ~/.git-prompt.sh
