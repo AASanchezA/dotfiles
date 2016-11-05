ec7f41f31d6adcdfcfc9999ba64ff893f66e0350
Unknown <andres-sanchez@gmx.net>
2016-11-05 18:35:35 +0100

    Add config for nodejs to avoid the need of sudo

--- installPackages.sh
+++ installPackages.sh
@@ -5,3 +5,13 @@ sudo apt-get install zsh git \
 	python python-pip ipython \
 	nodejs npm \
 	build-essential ctags
+
+# For Nodejs you need to do some changes
+mkdir ~/.npm-global
+npm config set prefix '~/.npm-global'
+
+# To Update nodejs using npm
+#sudo npm cache clean -f
+#sudo npm install -g n
+#sudo n stable
+
